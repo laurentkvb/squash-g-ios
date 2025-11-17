01dfed1acdcbbfce8c8a54d4a3d8880eba558225
Laurent Kleering van Beerenbergh <laurent.kleeringvb@ordina.nl>
2025-11-17 20:20:38 +0100

    Initial Commit

---
+++ squash-g-ios/squash_g_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  squash_g_iosApp.swift
+//  squash-g-ios
+//
+//  Created by Laurent Kleering van Beerenbergh on 17/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct squash_g_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
